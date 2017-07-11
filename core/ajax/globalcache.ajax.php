62be5cbbfdf0760bcc49c084273763960b20f606
Halbout Michael <michaelhalbout@gmail.com>
2017-07-11 08:50:37 +0200

    Update globalcache.ajax.php

--- core/ajax/globalcache.ajax.php
+++ core/ajax/globalcache.ajax.php
@@ -6,6 +6,15 @@
 		if (!isConnect('admin')) {
 			throw new Exception(__('401 - Accès non autorisé', __FILE__));
 		}
+		if (init('action') == 'getCacheMonitor') {
+			$return = false;
+			$eqLogic=eqLogic::byId(init('id'));
+			if(is_object($eqLogic)){
+				$cache = cache::byKey('globalcache::Monitor::'.$eqLogic->getId());
+				$return=$cache->getValue('[]')
+			}
+			ajax::success($return);
+		}
 		throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
 	} catch (Exception $e) {
 		ajax::error(displayExeption($e), $e->getCode());
