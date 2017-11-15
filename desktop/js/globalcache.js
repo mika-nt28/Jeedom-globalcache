ada889cfd3d36b8de058bf4d533109af7e5ade09
Halbout Michael <michaelhalbout@gmail.com>
2017-11-15 13:24:50 +0100

    Update globalcache.js

--- desktop/js/globalcache.js
+++ desktop/js/globalcache.js
@@ -99,7 +99,7 @@ function addCmdToTable(_cmd) {
 				.append($('<i class="fa fa-question-circle tooltips" style="font-size : 1em;color:grey;">')
 				.attr('title','Saisisser la valeur par defaut de votre commande'))))
 		.append($('<div>')
-			.append($('<textarea class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="value">')));
+			.append($('<textarea class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="value">'))));
 	td.append($('<div>')
 		.append($('<label class="checkbox-inline">')
 			.append($('<input type="checkbox" class="cmdAttr" data-l1key="configuration" data-l2key="CR" checked>'))
