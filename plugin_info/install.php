d29962635fedd91b674ce883907b1455e1488403
Halbout Michael <michaelhalbout@gmail.com>
2017-12-13 10:28:35 +0100

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -5,9 +5,17 @@ function globalcache_install(){
 function globalcache_update(){
 	log::add('globalcache','debug','Lancement du script de mise a jours'); 
 	foreach(eqLogic::byType('globalcache') as $eqLogic){
-    foreach($eqLogic->getCmd() as $cmd){
-      $cmd->save();
-    }
+		switch($eqLogic->getConfiguration("type")){
+			case "ir":
+				$eqLogic->setConfiguration("type","IR");
+			break;
+			case "serial":
+				$eqLogic->setConfiguration("type","SERIAL");
+			break;
+			case "relay":
+				$eqLogic->setConfiguration("type","RELAY");
+			break;
+		}
 		$eqLogic->save();
 	}
 	log::add('globalcache','debug','Fin du script de mise a jours');
